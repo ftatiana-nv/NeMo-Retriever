c8e7e0814a9d581e50ad294f045ea8263c6844cb
Tatiana Frenklach <tfrenklach@nvidia.com>
2026-03-10 09:35:01 +0200

    remove bi

--- nemo_retriever/src/nemo_retriever/relational_db/description_suggestion/functions.py
+++ nemo_retriever/src/nemo_retriever/relational_db/description_suggestion/functions.py
@@ -1,36 +1,17 @@
 import logging
 import re
 
-from infra.Neo4jConnection import get_neo4j_conn
+# TODO how to do the connection
+from nemo_retriever.relational_db.infra.Neo4jConnection import get_neo4j_conn
 
-from enrichments.ai_services.functions import get_ontology_prompt
-from enrichments.description_suggestion.system_prompts import (
+
+from nemo_retriever.relational_db.description_suggestion.system_prompts import (
     term_system_prompt,
     attribute_system_prompt,
     metric_system_prompt,
     analysis_system_prompt,
     analysis_name_system_prompt,
     sql_system_prompt,
-    tableau_dashboard_system_prompt,
-    tableau_visual_system_prompt,
-    tableau_embed_ds_system_prompt,
-    tableau_workbook_system_prompt,
-    tableau_published_ds_system_prompt,
-    quicksight_dashboard_system_prompt,
-    quicksight_system_prompt,
-    quicksight_dataset_system_prompt,
-    sisense_dashboard_system_prompt,
-    sisense_visual_system_prompt,
-    sisense_datamodel_system_prompt,
-    sisense_table_system_prompt,
-    powerbi_dashboard_system_prompt,
-    powerbi_table_system_prompt,
-    powerbi_report_system_prompt,
-    powerbi_visual_system_prompt,
-    looker_dashboard_system_prompt,
-    looker_explore_visual_system_prompt,
-    looker_look_view_system_prompt,
-    field_system_prompt,
     system_intro_prompt,
     table_system_prompt,
     schema_system_prompt,
@@ -39,6 +20,33 @@ from enrichments.description_suggestion.system_prompts import (
 
 logger = logging.getLogger("description suggestion")
 conn = get_neo4j_conn()
+
+# TODO where is ontology coming from?
+def get_ontology_prompt(ontology):
+    if not ontology:
+        return ""
+
+    ontology_prompt = ""
+
+    company_overview = ontology.get("overview", "")
+    if company_overview:
+        ontology_prompt += f"Company overview: {company_overview}\n"
+
+    industry_list = ontology.get("industry", [])
+    if len(industry_list) > 0:
+        industry_label = "industry" if len(industry_list) == 1 else "industries"
+        industries = ", ".join(industry_list)
+        ontology_prompt += f"within the context of the {industries} {industry_label}\n"
+
+    definitions_list = ontology.get("dictionary", [])
+    if len(definitions_list) > 0:
+        ontology_prompt += "Here are the relevant industry definitions:\n"
+        dictionary_items = [
+            f"{item['name']}: {item['description']}" for item in definitions_list
+        ]
+        ontology_prompt += "\n".join(dictionary_items)
+
+    return ontology_prompt + "\n"
 
 
 def get_final_prompt(prompt, system_prompt, ontology):
@@ -138,335 +146,7 @@ def create_query_prompt(data, ontology):
     return prompt, system_prompt
 
 
-def create_ta_dashboard_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "project",
-        "workbook",
-        "dashboard",
-        "sibling_dshs",
-        "visuals",
-    }:
-        return None, None
 
-    prompt = (
-        f"The dashboard name: '{data['dashboard']}', project name: '{data['project']}', "
-        f"workbook name: '{data['workbook']}', other dashboards under the same workbook: {data['sibling_dshs']}, "
-        f"visuals: {data['visuals']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, tableau_dashboard_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_ta_visual_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "project",
-        "workbook",
-        "dashboard",
-        "visual",
-        "sibling_visuals",
-        "fields",
-    }:
-        return None, None
-
-    prompt = (
-        f"The visual name: '{data['visual']}', project name: '{data['project']}', "
-        f"workbook name: '{data['workbook']}', dashboard name: '{data['dashboard']}', "
-        f"other visuals under the same dashboard: {data['sibling_visuals']}, "
-        f"fields: {data['fields']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, tableau_visual_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_ta_embed_ds_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "project",
-        "workbook",
-        "embed_ds",
-        "sibling_emb_dses",
-        "fields",
-    }:
-        return None, None
-
-    prompt = (
-        f"The embedded datasource name: '{data['embed_ds']}', project name: '{data['project']}', "
-        f"workbook name: '{data['workbook']}', other embedded data sources names under the same workbook: {data['sibling_emb_dses']}, "
-        f"fields names under the embedded data source: {data['fields']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, tableau_embed_ds_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_ta_workbook_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "project",
-        "workbook",
-        "dashboards",
-        "visuals",
-        "embedded_dss",
-        "sibling_wbs",
-    }:
-        return None, None
-
-    prompt = (
-        f"The workbook name: '{data['workbook']}', project name: '{data['project']}', "
-        f"dashboards' names under the workbook: {data['dashboards']}, "
-        f"visuals names under the workbook: {data['visuals']}, "
-        f"datasources names under the workbook: {data['embedded_dss']}, "
-        f"other workbooks names under the same project: {data['sibling_wbs']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, tableau_workbook_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_ta_published_ds_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "project",
-        "pub_ds",
-        "fields",
-        "sibling_pub_dses",
-    }:
-        return None, None
-
-    prompt = (
-        f"The published datasource name: '{data['pub_ds']}', project name: '{data['project']}', "
-        f"fields names under the published data source: '{data['fields']}', "
-        f"other published data sources names under the same project: {data['sibling_pub_dses']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, tableau_published_ds_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_qs_dashboard_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "visuals",
-    }:
-        return None, None
-
-    prompt = f"The dashboard name: '{data['dashboard']}', visuals names under the dashboard: {data['visuals']}."
-    prompt, system_prompt = get_final_prompt(
-        prompt, quicksight_dashboard_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_qs_visual_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "item_name",
-        "item_label",
-        "visual",
-        "sibling_visuals",
-        "fields",
-    }:
-        return None, None
-
-    quicksight_visual_system_prompt = quicksight_system_prompt + (
-        f"You will be provided with the visual name, fields' names under the visual, "
-        f"{data['item_label']} name that contains the visual, and other visuals' names under the same {data['item_label']}.\n"
-        "Your job is to describe in up to 3 sentences the given visual's BI role to the user who understands BI."
-    )
-    prompt = (
-        f"The visual name: '{data['visual']}', fields names under the visual: {data['fields']}, "
-        f"{data['item_label']} name that contains the visual: '{data['item_name']}', "
-        f"other visuals names under the same {data['item_label']}: {data['sibling_visuals']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, quicksight_visual_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_qs_dataset_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dataset",
-        "fields",
-    }:
-        return None, None
-
-    prompt = f"The dataset name: '{data['dataset']}', fields' names under the dataset: {data['fields']}."
-    prompt, system_prompt = get_final_prompt(
-        prompt, quicksight_dataset_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_sis_dashboard_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "visuals",
-        "sibling_dshes",
-        "folder",
-    }:
-        return None, None
-
-    prompt = (
-        f"The dashboard name: '{data['dashboard']}', visuals names under the dashboard: {data['visuals']}, "
-        f"other dashboards names under the same folder: {data['sibling_dshes']}, and folder name: '{data['folder']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, sisense_dashboard_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_sis_visual_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "visual",
-        "fields",
-        "sibling_visuals",
-        "folder",
-    }:
-        return None, None
-
-    prompt = (
-        f"The visual name: '{data['visual']}', the dashboard name: '{data['dashboard']}', "
-        f"fields' names under the visual: {data['fields']}, other visuals' names under the same dashboard: {data['sibling_visuals']}, "
-        f"and folder name: '{data['folder']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, sisense_visual_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_sis_datamodel_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "datamodel",
-        "table_fields",
-    }:
-        return None, None
-
-    prompt = (
-        f"The data model name: '{data['datamodel']}', "
-        f"the list of tables with fields for each table: '{data['table_fields']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, sisense_datamodel_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_sis_table_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "datamodel",
-        "table",
-        "table_fields",
-        "sibling_tables",
-    }:
-        return None, None
-
-    prompt = (
-        f"The table name: '{data['table']}', fields names under the table: {data['table_fields']}, "
-        f"data model name: '{data['datamodel']}', and the list of tables under the same data model: {data['sibling_tables']}."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, sisense_table_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_powerbi_datasource_prompt(data, ontology):
-    prompt = (
-        f"The {data['datasource_type']} name: '{data['datasource_name']}', "
-        f"the list of tables with fields for each table: '{data['table_fields']}'."
-    )
-
-    print(prompt)
-    prompt, system_prompt = get_final_prompt(
-        prompt, sisense_datamodel_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_powerbi_dashboard_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "reports",
-        "sibling_dshes",
-        "workspace",
-    }:
-        return None, None
-
-    prompt = (
-        f"The dashboard name: '{data['dashboard']}', reports names under the dashboard: {data['reports']}, "
-        f"other dashboards names under the same workspace: {data['sibling_dshes']}, and workspace name: '{data['workspace']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, powerbi_dashboard_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_powerbi_table_prompt(data, ontology):
-    prompt = (
-        f"The table name: '{data['table']}', fields names under the table: {data['table_fields']}, "
-        f"{data['parent_type']} name: '{data['parent_name']}', and the list of tables under the same {data['parent_type']}: {data['sibling_tables']}."
-    )
-    print(prompt)
-    prompt, system_prompt = get_final_prompt(
-        prompt, powerbi_table_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_powerbi_report_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "report",
-        "visuals",
-    }:
-        return None, None
-
-    prompt = (
-        f"The report name: '{data['report']}', "
-        f"visuals' names under the report: {data['visuals']} "
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, powerbi_report_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_powerbi_visual_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "report",
-        "visual",
-        "sibling_visuals",
-        "fields",
-    }:
-        return None, None
-
-    prompt = (
-        f"The visual name: '{data['visual']}', the report name: '{data['report']}', "
-        f"fields' names under the visual: {data['fields']}, other visuals' names under the same report: {data['sibling_visuals']}, "
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, powerbi_visual_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_field_prompt(data, ontology):
-    if data is None or not data.keys() >= {"parent_name", "field_name", "fields"}:
-        return None, None
-
-    prompt = (
-        f"The specific field name: '{data['field_name']}', parent name: '{data['parent_name']}', "
-        f"a list of other field names within the same parent: {data['fields']}."
-    )
-    prompt, system_prompt = get_final_prompt(prompt, field_system_prompt, ontology)
-    return prompt, system_prompt
 
 
 def fix_response(descriptions: str):
@@ -483,63 +163,3 @@ def remove_sql_comments(sql_query: str):
 
     return sql_query
 
-
-def create_looker_dashboard_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "visuals",
-        "sibling_dshes",
-        "folder",
-    }:
-        return None, None
-
-    prompt = (
-        f"The dashboard name: '{data['dashboard']}', visuals names under the dashboard: {data['visuals']}, "
-        f"other dashboards names under the same folder: {data['sibling_dshes']}, and folder name: '{data['folder']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, looker_dashboard_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_looker_explore_visual_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "dashboard",
-        "visual",
-        "fields",
-        "sibling_visuals",
-        "folder",
-    }:
-        return None, None
-
-    prompt = (
-        f"The visual name: '{data['visual']}', the dashboard name: '{data['dashboard']}', "
-        f"fields' names under the visual: {data['fields']}, other visuals' names under the same dashboard: {data['sibling_visuals']}, "
-        f"and folder name: '{data['folder']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, looker_explore_visual_system_prompt, ontology
-    )
-    return prompt, system_prompt
-
-
-def create_looker_look_view_prompt(data, ontology):
-    if data is None or not data.keys() >= {
-        "model",
-        "visual",
-        "fields",
-        "sibling_visuals",
-        "folder",
-    }:
-        return None, None
-
-    prompt = (
-        f"The visual name: '{data['visual']}', the logical model name: '{data['model']}', "
-        f"fields' names under the visual: {data['fields']}, other visuals' names under the same logical model: {data['sibling_visuals']}, "
-        f"and folder name: '{data['folder']}'."
-    )
-    prompt, system_prompt = get_final_prompt(
-        prompt, looker_look_view_system_prompt, ontology
-    )
-    return prompt, system_prompt
